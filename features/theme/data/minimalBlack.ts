7f5bb2f9d04122081805a20ae9ab28bb3249699f
kevingida <kevingida@gmail.com>
2026-07-08 20:36:16 +0200

    add minimal black theme

---
+++ features/theme/data/minimalBlack.ts
@@ -0,0 +1,27 @@
+import { Theme } from "../interface";
+
+export const minimalBlack: Theme = {
+  id: "minimalBlack",
+
+  name: "Minimal Black",
+
+  wallpaper: "",
+
+  particles: "none",
+
+  music: "",
+
+  variables: {
+    "--background": "#131316",
+
+    "--surface-primary": "#F8F9FA",
+    "--surface-secondary": "",
+    "--surface-active": "#495057",
+
+    "--border-primary": "#495057",
+
+    "--text-primary": "#F8F9FA",
+    "--text-secondary": "#ADB5BD",
+    "--text-active": "",
+  },
+};
