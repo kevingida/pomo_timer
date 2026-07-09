5298813e6a1f9b780d6b371a4bccd7edfabc71b7
kevingida <kevingida@gmail.com>
2026-07-09 14:05:36 +0200

    add image background

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
 import ThemeSelector from "@/features/theme/components/ThemeSelector";
+import { useTheme } from "@/features/theme/hooks/useThemes";
 import Timer from "@/features/timer";
 
 export default function Home() {
+  const { theme } = useTheme();
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-background font-sans">
+    <div
+      className="flex flex-col flex-1 items-center justify-center bg-background font-sans bg-center bg-cover bg-no-repeat"
+      style={{
+        background: theme.variables["--background"]
+          ? theme.variables["--background"]
+          : `url(${theme.wallpaper}) center / cover no-repeat`,
+      }}
+    >
       <div className="absolute top-4 right-4">
         <ThemeSelector />
       </div>
