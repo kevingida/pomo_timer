e3e7950da592978beb98e5beeaa63b8f08e82826
kevingida <kevingida@gmail.com>
2026-07-27 14:03:45 +0200

    add dropdown component

---
+++ components/Dropdown.tsx
@@ -0,0 +1,86 @@
+import { ChevronDown, Check } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+
+export interface DropdownOption {
+  label: string;
+  value: string;
+}
+
+interface DropdownProps {
+  id: string;
+  value: string;
+  options: DropdownOption[];
+  onChange: (value: string) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+const Dropdown = ({
+  id,
+  value,
+  options,
+  onChange,
+  placeholder,
+  className,
+}: DropdownProps) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const selected = options.find((o) => o.value === value);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (!ref.current?.contains(e.target as Node)) {
+        setOpen(false);
+      }
+    };
+
+    window.addEventListener("mousedown", handleClick);
+    return () => window.removeEventListener("mousedown", handleClick);
+  }, []);
+
+  return (
+    <div ref={ref} className={`relative w-fit min-w-30 ${className}`}>
+      <button
+        type="button"
+        onClick={() => setOpen((v) => !v)}
+        className="flex w-full items-center justify-between rounded-xl border border-border-primary bg-white/5 px-4 py-2.5 backdrop-blur-lg transition-all hover:bg-white/10"
+        id={id}
+      >
+        <div className="flex items-center gap-2 text-text-primary">
+          {selected?.label ?? placeholder}
+        </div>
+
+        <ChevronDown
+          size={18}
+          className={`transition-transform ${open ? "rotate-180" : ""}`}
+        />
+      </button>
+
+      {open && (
+        <div className=" absolute left-0 top-full z-50 mt-2 w-full overflow-hidden rounded-xl border border-border-primary/30 bg-surface-active/95 backdrop-blur-lg shadow-2xl">
+          {options.map((option) => (
+            <button
+              key={option.value}
+              onClick={() => {
+                onChange(option.value);
+                setOpen(false);
+              }}
+              className="flex w-full items-center justify-between px-4 py-3 text-left transition-colors hover:bg-white/10 "
+            >
+              <div className="flex items-center gap-2 text-text-active">
+                {option.label}
+              </div>
+
+              {option.value === value && (
+                <Check size={16} className="text-text-active" />
+              )}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
